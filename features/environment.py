18c3624c84f16f658ace91b5be7478e7a19919db
Byran Wills-Heath <byran@adgico.co.uk>
2020-06-07 20:09:10 +0100

    Add selenium tasks

--- features/environment.py
+++ features/environment.py
@@ -2,6 +2,14 @@ from behave import runner, model
 from screenplay.behave_extensions import add_screenplay_objects_to
 from screenplay.log import Log
 from abilities.browse_the_web import browse_the_web
+from tasks_selenium import save_screenshot
+import os
+import pathlib
+
+
+def before_all(context: runner.Context):
+    repositoryRoot = pathlib.Path(__file__).parent.parent.absolute()
+    save_screenshot.path = os.path.join(repositoryRoot, 'docs', 'test_results')
 
 
 def before_scenario(context: runner.Context, scenario: model.Scenario):
