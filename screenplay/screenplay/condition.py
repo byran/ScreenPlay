80c6f09cf723aefd5bba92a94a603f97a8f2c13c
Byran Wills-Heath <byran@adgico.co.uk>
2020-06-07 20:59:09 +0100

    Add running Tasks/Actions on Condition success, failure or both

--- screenplay/screenplay/condition.py
+++ screenplay/screenplay/condition.py
@@ -6,24 +6,37 @@ class Condition:
     def __init__(self, question: Question, expected: Matcher):
         self.question = question
         self.expected = expected
+        self._successActions = []
         self._failureActions = []
+        self._alwaysActions = []
+
+    def if_they_do(self, *actions):
+        self._successActions.extend(actions)
+        return self
 
     def if_they_do_not(self, *actions):
         self._failureActions.extend(actions)
         return self
 
-    def _run_failure_actions_as(self, actor):
+    def regardless_of_that(self, *actions):
+        self._alwaysActions.extend(actions)
+        return self
+
+    def _run_actions_as(self, actor, actions):
         try:
-            actor.attempts_to(*self._failureActions)
+            actor.attempts_to(*actions)
         except:
             pass
 
     def check_as(self, actor):
         try:
             assert self.expected.matches(self.question.answered_by(actor)), self.expected.fail_message
+            self._run_actions_as(actor, self._successActions)
         except Exception as exception:
-            self._run_failure_actions_as(actor)
+            self._run_actions_as(actor, self._failureActions)
             raise exception
+        finally:
+            self._run_actions_as(actor, self._alwaysActions)
 
 
 see_that = Condition
