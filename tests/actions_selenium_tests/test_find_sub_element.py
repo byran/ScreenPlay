1b0ef39009e930b64e561eaabf22379f4ecf42a8
Byran Wills-Heath <byran@adgico.co.uk>
2020-06-14 18:19:00 +0100

    Add tests for click_on_subelement and find_sub_element

---
+++ tests/actions_selenium_tests/test_find_sub_element.py
@@ -0,0 +1,24 @@
+from screenplay import Actor
+from abilities import browse_the_web
+from actions_selenium import navigate_to, find_element, find_sub_element
+from os import path
+from selenium.webdriver.common.by import By
+
+
+test_page = 'file://' + path.join(path.dirname(__file__), 'elements.html')
+
+
+def test_finding_a_subelement_of_a_stored_element_is_stored_and_returned():
+    user = Actor.named('user').who_can(browse_the_web.using_Chrome())
+
+    returned_value = user.attempts_to(
+        navigate_to(test_page),
+        find_element((By.ID, 'parent')).and_store_as('parent'),
+        find_sub_element((By.ID, 'child')).from_stored_element('parent').and_store_as('child')
+    )
+
+    assert returned_value.text == 'child'
+
+    stored_value = user.state['child'].value
+
+    assert stored_value.text == 'child'
