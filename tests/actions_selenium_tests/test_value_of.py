3b57d8ca7244e882e3385463b76b23837bec26ee
Byran Wills-Heath <byran@adgico.co.uk>
2020-06-14 15:58:05 +0100

    Add tests for missed code in enter_text and value_of

--- tests/actions_selenium_tests/test_value_of.py
+++ tests/actions_selenium_tests/test_value_of.py
@@ -1,6 +1,6 @@
 from screenplay import Actor
 from abilities import browse_the_web
-from actions_selenium import navigate_to, find_element, value_of
+from actions_selenium import navigate_to, find_element, enter_text, value_of
 from os import path
 from selenium.webdriver.common.by import By
 
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 test_page = 'file://' + path.join(path.dirname(__file__), 'elements.html')
 
 
-def test_text_can_be_entered_into_a_stored_element():
+def test_the_value_of_a_stored_element_is_returned():
     user = Actor.named('user').who_can(browse_the_web.using_Chrome())
 
     returned_value = user.attempts_to(
@@ -18,3 +18,18 @@ def test_text_can_be_entered_into_a_stored_element():
     )
 
     assert returned_value == 'fourth textbox'
+
+
+def test_a_whitespace_stripped_value_of_a_stored_element_is_returned():
+    user = Actor.named('user').who_can(browse_the_web.using_Chrome())
+
+    locator = (By.ID, 'third_text')
+
+    returned_value = user.attempts_to(
+        navigate_to(test_page),
+        enter_text('  text  ').into_element(locator),
+        find_element(locator).and_store_as('textbox'),
+        value_of().stored_element('textbox').stripped_of_whitespace()
+    )
+
+    assert returned_value == 'text'
