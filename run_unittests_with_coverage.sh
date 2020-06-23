c4a9262a282e120cf247f4e1379c1715639279da
Byran Wills-Heath <byran@adgico.co.uk>
2020-06-23 23:20:26 +0100

    Refactor running unit tests

--- run_unittests_with_coverage.sh
+++ run_unittests_with_coverage.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-coverage run --branch --module pytest --junit-xml=unittest_results.xml
+readonly SCRIPT_PATH=$(realpath $(dirname $0))
+cd "$SCRIPT_PATH"
+
+source .venv/bin/activate
+
+coverage run --branch --module pytest --junit-xml=docs_scripts/unittest_results.xml
 readonly EXIT_CODE=$?
 
 coverage report --omit=".venv/*" --show-missing
 
 coverage html --omit=".venv/*" -d docs/_static/coverage
 
-./create_unittest_results_sphinx_page.py unittest_results.xml docs/unittest_results/unittest_results.rst
+docs_scripts/create_unittest_results_sphinx_page.py docs_scripts/unittest_results.xml docs/unittest_results/unittest_results.rst
 
 exit ${EXIT_CODE}
