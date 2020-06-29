4af01f83b9e4d9a9dae3a58106702321e81a8941
Byran Wills-Heath <byran@adgico.co.uk>
2020-06-30 00:25:59 +0100

    Make linting on Linux fail if there are only warnings

--- lint.sh
+++ lint.sh
@@ -8,4 +8,4 @@ echo Checking for syntax errors
 flake8 . --exclude=.venv --count --select=E9,F63,F7,F82 --show-source --statistics
 
 echo Linting
-flake8 . --exclude=.venv --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --ignore=F811
+flake8 . --exclude=.venv --count --max-complexity=10 --max-line-length=127 --statistics --ignore=F811
