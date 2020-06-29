6ec9191d61e130e577bfd28fda17e628dc601197
Byran Wills-Heath <byran@adgico.co.uk>
2020-06-29 22:29:09 +0100

    Add pause_for to actions_common/__init__.py

--- src/actions_common/__init__.py
+++ src/actions_common/__init__.py
@@ -1,10 +1,12 @@
 from .fail_with_message import fail_with_message
 from .if_value_of import if_value_of
+from .pause_for import pause_for
 from .select_element_at_index import select_element_at_index
 
 
 __all__ = [
     'fail_with_message',
     'if_value_of',
+    'pause_for',
     'select_element_at_index'
 ]
