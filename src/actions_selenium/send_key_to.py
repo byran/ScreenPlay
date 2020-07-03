34e61cbe8a63de3029761f0f7529f00fe6f10653
Byran Wills-Heath <byran@adgico.co.uk>
2020-07-03 18:02:04 +0100

    Add send_CTRL action to actions_selenium

--- src/actions_selenium/send_key_to.py
+++ src/actions_selenium/send_key_to.py
@@ -3,6 +3,7 @@
 from screenplay import Action, Actor, log_message
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from abilities.browse_the_web import browser_for
 
 
@@ -52,3 +53,50 @@ class send_enter_key_to:
         id - The Id of the stored element
         """
         return _send_key_to_stored_element(id, Keys.ENTER, 'Enter')
+
+
+class _send_CTRL_plus_key_to_element(Action):
+    def __init__(self, locator, key, key_name: str):
+        super().__init__()
+        self._locator = locator
+        self._key = key
+        self._key_name = key_name
+
+    @log_message('Send key "CTRL+{self._key_name}" to element "{self._locator}"')
+    def perform_as(self, actor: Actor):
+        browser = browser_for(actor)
+        element: WebElement = browser.find_element(*self._locator)
+        chain = ActionChains(browser)
+        chain.key_down(Keys.LEFT_CONTROL, element=element)
+        chain.send_keys_to_element(element, self.key)
+        chain.key_up(Keys.LEFT_CONTROL, element=element)
+        chain.perform()
+
+
+class _send_CTRL_plus_key_to_stored_element(Action):
+    def __init__(self, id, key, key_name: str):
+        super().__init__()
+        self._id = id
+        self._key = key
+        self._key_name = key_name
+
+    @log_message('Send key "CTRL+{self._key_name}" to element "{self._id}"')
+    def perform_as(self, actor: Actor):
+        browser = browser_for(actor)
+        element = actor.state[self._id].value
+        chain = ActionChains(browser)
+        chain.key_down(Keys.LEFT_CONTROL, element=element)
+        chain.send_keys_to_element(element, self.key)
+        chain.key_up(Keys.LEFT_CONTROL, element=element)
+        chain.perform()
+
+
+class send_CTRL:
+    def __init__(self, key: str):
+        self.key = key
+
+    def to_element(self, locator):
+        return _send_CTRL_plus_key_to_element(locator, self.key, self.key)
+
+    def to_stored_element(self, id: str):
+        return _send_CTRL_plus_key_to_stored_element(id, self.key, self.key)
