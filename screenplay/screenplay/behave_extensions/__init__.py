7374f7d45f2a3951cd788263fb75405bb6689376
Byran Wills-Heath <byran@adgico.co.uk>
2020-07-14 23:05:32 +0100

    Add ScreenPlay JSON formatter

--- screenplay/screenplay/behave_extensions/__init__.py
+++ screenplay/screenplay/behave_extensions/__init__.py
@@ -1,6 +1,13 @@
 from .actors import Actors
 from .context import add_screenplay_objects_to
+from .json_formatter import JSONFormatter
 from .formatter import ScreenplayFormatter
 from .sphinx_formatter import SphinxScreenplayFormatter
 
-__all__ = ['Actors', 'add_screenplay_objects_to', 'ScreenplayFormatter', 'SphinxScreenplayFormatter']
+__all__ = [
+    'Actors',
+    'add_screenplay_objects_to',
+    'JSONFormatter',
+    'ScreenplayFormatter',
+    'SphinxScreenplayFormatter'
+]
