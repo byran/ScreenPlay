3743c91972085107eb1684cc91c8a7da7f68aba8
Byran Wills-Heath <byran@adgico.co.uk>
2021-04-08 23:29:12 +0100

    Change rstjinja package to support all output types, not just HTML

--- rstjinja/rstjinja/__init__.py
+++ rstjinja/rstjinja/__init__.py
@@ -1,17 +1,8 @@
-"""
-Jinja support in Sphinx
-Based on https://www.ericholscher.com/blog/2016/jul/25/integrating-jinja-rst-sphinx/
-"""
+from jinja2 import Template
 
 
 def rstjinja(app, docname, source):
-    # Make sure we're outputting HTML
-    if app.builder.format != 'html':
-        return
-    rendered = app.builder.templates.render_string(
-        source[0], app.config.html_context
-    )
-    source[0] = rendered
+    source[0] = Template(source[0]).render(**app.config.html_context)
 
 
 def setup(app):
