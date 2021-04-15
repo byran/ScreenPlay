9fd812284e9d5960e0c535824a75a094cb105a2f
Byran Wills-Heath <byran@adgico.co.uk>
2021-04-15 23:06:22 +0100

    Refactor sourcecode metrics script so it can be called from anywhere

--- docs_scripts/create_sourcecode_metrics_sphinx_page.py
+++ docs_scripts/create_sourcecode_metrics_sphinx_page.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python3
 import sys
 import json
+import pathlib
+from os import path
 from jinja2 import Template
+
+
+my_path = pathlib.Path(__file__).parent.absolute()
+
+
+def read_datafile(filename: str):
+    filepath = path.join(my_path, filename)
+    if not path.exists(filepath):
+        print('Unable to find {filename}\n'.format(filename=filename))
+        exit(2)
+
+    with open(filepath) as file:
+        return json.load(file)
+
+
+def read_template():
+    filepath = path.join(my_path, 'metrics.template')
+    if not path.exists(filepath):
+        print('Unable to find template\n')
+        exit(2)
+
+    with open(filepath) as file:
+        return file.read()
 
 
 def non_class_complexity(file):
@@ -17,24 +42,17 @@ if __name__ == "__main__":
         print('Invalid arguments:\n{cmd} <output file>\n'.format(cmd=sys.argv[0]))
         exit(1)
 
-    with open('mi.json') as mi:
-        maintainability = json.load(mi)
-
-    with open('cc.json') as cc:
-        complexity = json.load(cc)
-
-    with open('raw.json') as raw:
-        common = json.load(raw)
+    mi = read_datafile('mi.json')
+    cc = read_datafile('cc.json')
+    raw = read_datafile('raw.json')
+    template = read_template()
 
     context = {
-        "mi": maintainability,
-        "cc": complexity,
-        "raw": common,
+        "mi": mi,
+        "cc": cc,
+        "raw": raw,
         "non_class_complexity": non_class_complexity
     }
-
-    with open('metrics.template') as file:
-        template = file.read()
 
     with open(sys.argv[1], 'wt') as file:
         file.write(Template(template).render(**context))
