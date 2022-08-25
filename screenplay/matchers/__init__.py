db994ff5fe1e36eeaa067fb63090f48f986d5923
Byran Wills-Heath <byran@adgico.co.uk>
2022-08-25 14:02:07 +0100

    Fix missing export of is_boolean functions in screenplay/matchers/__init__.py

--- screenplay/matchers/__init__.py
+++ screenplay/matchers/__init__.py
@@ -1,9 +1,12 @@
 from .contains import contains
 from .does_not_contain import does_not_contain
 from .equals import is_, equals
+from .is_boolean import is_true, is_false
+
 
 __all__ = [
     'contains',  # contains.py
     'does_not_contain',  # does_not_contain.py
-    'is_', 'equals'  # equals.py
+    'is_', 'equals',  # equals.py
+    'is_true', 'is_false'  # is_boolean.py
 ]
