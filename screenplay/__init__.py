e774bffa751a3efcb1c20df004d2b0cded9be5d7
Byran Wills-Heath <byran@adgico.co.uk>
2022-08-25 01:13:17 +0100

    Add missing Matcher export in screenplay/__init__.py

--- screenplay/__init__.py
+++ screenplay/__init__.py
@@ -4,8 +4,9 @@ from .actor_state import value_of
 from .actor import Actor
 from .condition import see_that
 from .question import Question
+from .matcher import Matcher
 from .task import Task
 from .log import log_message
 from .using import using
 
-__all__ = ['Ability', 'Action', 'value_of', 'Actor', 'see_that', 'Question', 'Task', 'log_message', 'using']
+__all__ = ['Ability', 'Action', 'value_of', 'Actor', 'see_that', 'Question', 'Matcher', 'Task', 'log_message', 'using']
